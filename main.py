8b3b15cb13bf16798eece19afcd368e4638f7393
Patrice Waugh <104030397+PattyWau@users.noreply.github.com>
2022-08-07 21:03:24 -0400

    Add files via upload
    add postgresql and environ variables

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ Bootstrap(app)
 Base = declarative_base()
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(DATABASE_URL)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
